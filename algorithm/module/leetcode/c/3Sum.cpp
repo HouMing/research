ad0e0752455ba2250913513f65dea393e9504c1c
HouMing <56071912@qq.com>
2013-01-20 23:42:19 +0800

    TODO, AddTwoNumbers.fix up 3Sum

--- algorithm/module/leetcode/c/3Sum.cpp
+++ algorithm/module/leetcode/c/3Sum.cpp
@@ -1,33 +1,55 @@
-// http://leetcode.com/2012/05/clone-graph-part-i.html
-/*
- * Given an array S of n integers, are there elements a, b, c in S such that a +
- * b + c = 0? Find all unique triplets in the array which gives the sum of zero.
- *
- * Note:
- * Elements in a triplet (a,b,c) must be in non-descending order. (ie, a ≤ b ≤ c)
- * The solution set must not contain duplicate triplets.
- * For example, given array S = {-1 0 1 2 -1 -4},
- *
- * A solution set is:
- * (-1, 0, 1)
- * (-1, -1, 2)
- */
 #include <iostream>
 #include <vector>
+#include <algorithm>
+
 using namespace std;
-//
-//class Solution
-//{
-// public:
-//  vector<vector<int> > threeSum(vector<int> &sum)
-//  {
-//  vector<vector<int> > ret = new vector<vector<int> >();
-//    return NULL;
-//  }
-//};
-//
+class Solution
+{
+ public:
+  vector<vector<int> > threeSum(vector<int> &num)
+  {
+    vector<vector<int> > ret;
+    vector<int>::size_type size = num.size();
+    sort(num.begin(), num.end());
+    int cur_sum;
+    for (vector<int>::size_type i = 0; i != size-2 ; ++i) {
+      vector<int>::size_type l = i + 1;
+      vector<int>::size_type r = size - 1;
+      cur_sum = num[i] + num[l] + num[r];
+      while (l < r) {
+        cur_sum = num[i] + num[l] + num[r];
+        if (cur_sum == 0) {
+          vector<int> vc;
+          vc.push_back(num[l]);
+          vc.push_back(num[i]);
+          vc.push_back(num[r]);
+          ret.push_back(vc);
+          ++l;
+        }
+        if (cur_sum < 0) {
+          ++l;
+        }
+        if (cur_sum > 0) {
+          --r;
+        }
+      }
+    }
+    return ret;
+  }
+};
+
 int main() {
   // Start typing your code here...
-  cout << "Hello world!" << endl;
+  cout << "3Sum:" << endl;
+  int arr[] = {-1, 0, 1, 2, -1, -4};
+  vector<int> tmp(arr,arr + 7);
+  Solution sln;
+  vector<vector<int> > trmp =  sln.threeSum(tmp);
+  for (vector<vector<int> >::size_type i = 0; i < trmp.size(); ++i) {
+    for (vector<int>::size_type j = 0; j < trmp[i].size() ; ++j) {
+      cout << trmp[i][j] << endl;
+    }
+  }
+  cout << "!3Sum:" << endl;
   return 0;
 }
--- algorithm/module/leetcode/c/3SumClosest.cpp
+++ algorithm/module/leetcode/c/3SumClosest.cpp
@@ -1,17 +1,4 @@
 // http://leetcode.com/2012/05/clone-graph-part-i.html
-/*
- * Given an array S of n integers, are there elements a, b, c in S such that a +
- * b + c = 0? Find all unique triplets in the array which gives the sum of zero.
- *
- * Note:
- * Elements in a triplet (a,b,c) must be in non-descending order. (ie, a ≤ b ≤ c)
- * The solution set must not contain duplicate triplets.
- * For example, given array S = {-1 0 1 2 -1 -4},
- *
- * A solution set is:
- * (-1, 0, 1)
- * (-1, -1, 2)
- */
 #include <iostream>
 #include <vector>
 #include <algorithm>
